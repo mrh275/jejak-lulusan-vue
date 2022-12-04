47dd4aa0bf08e51e5567fc1c3d8caa9315dab7e0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-04 08:05:27 +0700

    import font awesome to libray as component

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faBars,
+  faUser,
+  faUserSecret,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret, faUser, faBars);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component("fa", FontAwesomeIcon);
 app.mount("#app");
