19a635a9a3c5b9fb3d6c813fd2ffc9e21a939ddf
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-04 08:05:47 +0700

    added header to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import HeaderVue from "./components/Header.vue";
 </script>
 
 <template>
+  <header-vue></header-vue>
   <RouterView />
 </template>
